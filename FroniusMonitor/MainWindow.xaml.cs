6f3a4c9527d0501776abd9784e52d000791bd8b3
Christoph Hochstätter <christoh@hotmail.com>
2021-11-12 22:46:50 +0100

    Project creation

---
+++ FroniusMonitor/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace De.Hochstaetter.FroniusMonitor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
