a2c6cf6708955bc7ef13f5e032dbf275442dad29
Christoph Hochstätter <christoh@hotmail.com>
2021-12-30 00:12:26 +0100

    Removed redundant code

--- FroniusMonitor/Wpf/Converters/Converters.cs
+++ FroniusMonitor/Wpf/Converters/Converters.cs
@@ -1,9 +1,7 @@
 ﻿using System.Globalization;
-using System.Windows;
 using System.Windows.Data;
 using System.Windows.Markup;
 using System.Windows.Media;
-using De.Hochstaetter.Fronius.Contracts;
 using De.Hochstaetter.Fronius.Models;
 using De.Hochstaetter.FroniusMonitor.Unity;
 using De.Hochstaetter.FroniusMonitor.Models;
@@ -22,25 +20,6 @@ public abstract class ConverterBase : MarkupExtension, IValueConverter
     }
 }
 
-public class TreeViewConverter : ConverterBase
-{
-    public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        if (!(value is IHierarchicalCollection hierarchicalCollection))
-        {
-            return null!;
-        }
-
-        var result = new CompositeCollection
-            {
-                new CollectionContainer {Collection = hierarchicalCollection.ItemsEnumerable},
-                new CollectionContainer {Collection = hierarchicalCollection.ChildrenEnumerable},
-            };
-
-        return result;
-    }
-}
-
 public class NullToString : ConverterBase
 {
     public string NullText { get; init; } = "---";
@@ -48,41 +27,6 @@ public class NullToString : ConverterBase
     public override object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         return value ?? NullText;
-    }
-}
-
-public class PowerDirectionToAnything<T> : ConverterBase
-{
-    public T? Incoming { get; set; }
-    public T? Outgoing { get; set; }
-    public T? Null { get; set; } = default;
-
-    public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        if (value is not IConvertible convertible)
-        {
-            return Null;
-        }
-
-        var doubleVal = convertible.ToDouble(culture);
-        return doubleVal < 0 ? Outgoing : doubleVal > 0 ? Incoming : Null;
-    }
-}
-
-public class PowerDirectionToDouble : PowerDirectionToAnything<double> { }
-
-public class PowerDirectionToThickness : PowerDirectionToAnything<Thickness> { }
-
-public class ToAbsolute : ConverterBase
-{
-    public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        if (value is not IConvertible convertible)
-        {
-            return null;
-        }
-
-        return Math.Abs(convertible.ToDouble(culture));
     }
 }
 
