b5c5a772c894c7aa396fc9359f2f76e44c1d012e
Christoph Hochstätter <Christoph@Hochstaetter.de>
2022-01-05 18:28:52 +0100

    Started FritzBox color control

---
+++ Fronius/Models/FritzBoxColorControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace De.Hochstaetter.Fronius.Models;
+
+[Flags]
+public enum FritzBoxColorMode
+{
+    Hsv = 1,
+    Temperature = 4,
+}
+
+[XmlType("colorcontrol")]
+public class FritzBoxColorControl:BindableBase
+{
+    private FritzBoxColorMode supportedModes;
+
+    public FritzBoxColorMode SupportedModes
+    {
+        get => supportedModes;
+        set => Set(ref supportedModes, value);
+    }
+}
