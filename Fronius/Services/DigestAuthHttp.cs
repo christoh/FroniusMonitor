a9f7b603578e59ee5c42580c763c23397a71e920
Christoph Hochstätter <Christoph@Hochstaetter.de>
2022-05-31 00:26:45 +0200

    Some digest auth improvements

--- Fronius/Services/DigestAuthHttp.cs
+++ Fronius/Services/DigestAuthHttp.cs
@@ -2,6 +2,7 @@
 {
     public class DigestAuthHttp
     {
+        private static readonly Random random = new(unchecked((int)DateTime.UtcNow.Ticks));
         private readonly string host;
         private readonly string user;
         private readonly string password;
@@ -16,7 +17,7 @@
         {
             host = connection.BaseUrl ?? "/";
             user = connection.UserName ?? "customer";
-            password = connection.Password ?? string.Empty;
+            password = connection.Password;
         }
 
         public async Task<string> GetString(string dir, JToken? token = null)
@@ -34,18 +35,18 @@
             await AddToken(request, token).ConfigureAwait(false);
 
             // If we've got a recent Auth header, re-use it!
-            if (!string.IsNullOrEmpty(cnonce) && DateTime.Now.Subtract(cnonceDate).TotalHours < 1.0)
+            if (!string.IsNullOrEmpty(cnonce) && DateTime.UtcNow.Subtract(cnonceDate).TotalHours < 1.0)
             {
                 request.Headers.Add("Authorization", await CreateDigestHeader(dir, isPost).ConfigureAwait(false));
             }
 
-            HttpWebResponse? response = null;
+            WebResponse? response = null;
 
             try
             {
                 try
                 {
-                    response = (HttpWebResponse)await request.GetResponseAsync().ConfigureAwait(false);
+                    response = await request.GetResponseAsync().ConfigureAwait(false);
                 }
                 catch (WebException ex)
                 {
@@ -61,16 +62,16 @@
                     qop = await GetAuthHeaderToken("qop", wwwAuthenticateHeader).ConfigureAwait(false);
 
                     nc = 0;
-                    cnonce = new Random().Next(123400, 9999999).ToString();
-                    cnonceDate = DateTime.Now;
+                    cnonce = random.Next(123400, 9999999).ToString();
+                    cnonceDate = DateTime.UtcNow;
 #pragma warning disable CS0618
 #pragma warning disable SYSLIB0014 // Type or member is obsolete
-                    var request2 = (HttpWebRequest)WebRequest.Create(uri);
+                    var request2 = WebRequest.Create(uri);
                     await AddToken(request2, token).ConfigureAwait(false);
 #pragma warning restore CS0618
 #pragma warning restore SYSLIB0014 // Type or member is obsolete
                     request2.Headers.Add("Authorization", await CreateDigestHeader(dir, isPost).ConfigureAwait(false));
-                    response = (HttpWebResponse)await request2.GetResponseAsync().ConfigureAwait(false);
+                    response = await request2.GetResponseAsync().ConfigureAwait(false);
                 }
 
                 using var reader = new StreamReader(response.GetResponseStream());
@@ -82,7 +83,7 @@
             }
         }
 
-        private async Task AddToken(HttpWebRequest request, JToken? token)
+        private static async Task AddToken(WebRequest request, JToken? token)
         {
             if (token == null)
             {
@@ -91,25 +92,24 @@
 
             request.Method = "POST";
             request.ContentType = "application/json";
-            await using var streamWriter = new StreamWriter(request.GetRequestStream());
+            await using var streamWriter = new StreamWriter(await request.GetRequestStreamAsync().ConfigureAwait(false));
             await streamWriter.WriteAsync(token.ToString()).ConfigureAwait(false);
         }
 
-        private string CalculateMd5Hash(string input)
+        private static string CalculateMd5Hash(string input)
         {
-            var inputBytes = Encoding.ASCII.GetBytes(input);
-            var hash = MD5.Create().ComputeHash(inputBytes);
-            var sb = new StringBuilder();
+            var hash = MD5.Create().ComputeHash(Encoding.ASCII.GetBytes(input));
 
-            foreach (var b in hash)
-            {
-                sb.Append(b.ToString("x2"));
-            }
-
-            return sb.ToString();
+            return hash
+                .Aggregate
+                (
+                    new StringBuilder(hash.Length << 1),
+                    (stringBuilder, hashByte) => stringBuilder.Append(hashByte.ToString("x2"))
+                )
+                .ToString();
         }
 
-        private Task<string> GetAuthHeaderToken(string varName, string header) => Task.Run(() =>
+        private static Task<string> GetAuthHeaderToken(string varName, string header) => Task.Run(() =>
         {
             var regHeader = new Regex($@"{varName}=""([^""]*)""");
             var matchHeader = regHeader.Match(header);
@@ -125,10 +125,9 @@
         [SuppressMessage("ReSharper", "StringLiteralTypo")]
         private Task<string> CreateDigestHeader(string dir, bool isPost) => Task.Run(() =>
         {
-            nc += 1;
             var ha1 = CalculateMd5Hash($"{user}:{realm}:{password}");
             var ha2 = CalculateMd5Hash($"{(isPost ? "POST" : "GET")}:{dir}");
-            var digestResponse = CalculateMd5Hash($"{ha1}:{nonce}:{nc:00000000}:{cnonce}:{qop}:{ha2}");
+            var digestResponse = CalculateMd5Hash($"{ha1}:{nonce}:{++nc:00000000}:{cnonce}:{qop}:{ha2}");
             return $"Digest username=\"{user}\", realm=\"{realm}\", nonce=\"{nonce}\", uri=\"{dir}\", " + $"algorithm=MD5, response=\"{digestResponse}\", qop={qop}, nc={nc:00000000}, cnonce=\"{cnonce}\"";
         });
     }
