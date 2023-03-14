a27d2c7faf30ac37c3b9a8c4bd03c43275199be2
Christoph Hochstätter <Christoph@Hochstaetter.de>
2023-03-14 03:20:09 +0100

    Added Control for A/C modes

---
+++ FroniusMonitor/Controls/ToshibaAcControl.xaml.cs
@@ -0,0 +1,65 @@
+﻿namespace De.Hochstaetter.FroniusMonitor.Controls;
+
+public partial class ToshibaAcControl
+{
+    private readonly ISolarSystemService solarSystemService = null!;
+    private CancellationTokenSource? enablerTokenSource;
+
+    public static readonly DependencyProperty DeviceProperty = DependencyProperty.Register
+    (
+        nameof(Device), typeof(ToshibaAcMappingDevice), typeof(ToshibaAcControl),
+        new PropertyMetadata((d, e) => ((ToshibaAcControl)d).OnDeviceChanged(e))
+    );
+
+    public ToshibaAcMappingDevice Device
+    {
+        get => (ToshibaAcMappingDevice)GetValue(DeviceProperty);
+        set => SetValue(DeviceProperty, value);
+    }
+
+    public ToshibaAcControl()
+    {
+        InitializeComponent();
+
+        if (!DesignerProperties.GetIsInDesignMode(this))
+        {
+            solarSystemService = IoC.Get<ISolarSystemService>();
+        }
+
+        Unloaded += (_, _) => Device.State.PropertyChanged -= OnStateChanged;
+    }
+
+    private void OnDeviceChanged(DependencyPropertyChangedEventArgs e)
+    {
+        if (e.OldValue is ToshibaAcMappingDevice oldDevice)
+        {
+            oldDevice.State.PropertyChanged -= OnStateChanged;
+        }
+
+        if (e.NewValue is ToshibaAcMappingDevice newDevice)
+        {
+            newDevice.State.PropertyChanged += OnStateChanged;
+        }
+    }
+
+    private void OnStateChanged(object? sender = null, PropertyChangedEventArgs? e = null) => Dispatcher.InvokeAsync(() =>
+    {
+        enablerTokenSource?.Cancel();
+        enablerTokenSource?.Dispose();
+        enablerTokenSource = null;
+        return IsEnabled = true;
+    });
+
+    private void SendCommand(Func<ToshibaAcStateData> func)
+    {
+        IsEnabled = false;
+        var stateData = func();
+        solarSystemService.AcService.SendDeviceCommand(stateData, Device);
+        enablerTokenSource = new CancellationTokenSource();
+        Task.Delay(TimeSpan.FromSeconds(10), enablerTokenSource.Token).ContinueWith(_ => OnStateChanged(), enablerTokenSource.Token);
+    }
+
+    private void OnPowerClicked(object sender, RoutedEventArgs e) => SendCommand(() => new ToshibaAcStateData { IsTurnedOn = !Device.State.IsTurnedOn });
+
+    private void OnModeClicked(object sender, RoutedEventArgs e) => SendCommand(() => new ToshibaAcStateData { Mode = ((HvacIcon)sender).Mode });
+}
