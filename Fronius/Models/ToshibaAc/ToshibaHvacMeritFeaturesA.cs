2b91301eb4e8119121e6c3057c04e35728d61666
Christoph Hochstätter <christoph.hochstaetter@leica-geosystems.com>
2023-04-25 11:47:47 +0200

    file scoped namespace

--- Fronius/Models/ToshibaAc/ToshibaHvacMeritFeaturesA.cs
+++ Fronius/Models/ToshibaAc/ToshibaHvacMeritFeaturesA.cs
@@ -1,15 +1,14 @@
-﻿namespace De.Hochstaetter.Fronius.Models.ToshibaAc
+﻿namespace De.Hochstaetter.Fronius.Models.ToshibaAc;
+
+public enum ToshibaHvacMeritFeaturesA : byte // Nibble (half byte): Can only be from 0 to 15
 {
-    public enum ToshibaHvacMeritFeaturesA : byte // Nibble (half byte): Can only be from 0 to 15
-    {
-        None = 0,
-        HighPower = 1,
-        Silent1 = 2,
-        Eco = 3,
-        Heating8C = 4,
-        SleepCare = 5,
-        Floor = 6,
-        Comfort = 7,
-        Silent2 = 10,
-    }
+    None = 0,
+    HighPower = 1,
+    Silent1 = 2,
+    Eco = 3,
+    Heating8C = 4,
+    SleepCare = 5,
+    Floor = 6,
+    Comfort = 7,
+    Silent2 = 10,
 }
