e43f0fc6756982a6aa45303116ab369063f7d7c3
Christoph Hochstätter <Christoph@Hochstaetter.de>
2025-04-07 02:38:47 +0200

    Updated NuGet packages

--- HomeAutomationClient/HomeAutomationClient.Desktop/Program.cs
+++ HomeAutomationClient/HomeAutomationClient.Desktop/Program.cs
@@ -1,23 +1,21 @@
-﻿using Avalonia;
-using System;
-using De.Hochstaetter.HomeAutomationClient;
+﻿using System;
+using Avalonia;
 
-namespace HomeAutomationClient.Desktop
+namespace De.Hochstaetter.HomeAutomationClient.Desktop;
+
+internal sealed class Program
 {
-    internal sealed class Program
-    {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
 
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .WithInterFont()
-                .LogToTrace();
-    }
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
 }
