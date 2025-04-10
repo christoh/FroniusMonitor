26743e29158396f5a8966784cb80316845f7ed71
Christoph Hochstätter <Christoph@Hochstaetter.de>
2025-04-10 20:00:51 +0200

    Change cache file format in Desktop App

--- HomeAutomationClient/HomeAutomationClient.Desktop/Platform/Cache.cs
+++ HomeAutomationClient/HomeAutomationClient.Desktop/Platform/Cache.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -9,16 +11,16 @@ namespace De.Hochstaetter.HomeAutomationClient.Desktop.Platform;
 
 public class Cache : ICache
 {
-    private static readonly string cacheDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Hochstaetter", "HomeAutomationClient");
+    private static readonly string cacheDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Hochstätter", "HomeAutomationClient");
     private static readonly string cacheFilePath = Path.Combine(cacheDirectory, "cache.json");
 
     public Cache()
     {
         Directory.CreateDirectory(cacheDirectory);
-        
+
         if (!File.Exists(cacheFilePath))
         {
-            File.WriteAllText(cacheFilePath, "{}");
+            using var _ = File.Create(cacheFilePath);
         }
     }
 
@@ -50,25 +52,38 @@ public class Cache : ICache
 
     private static Dictionary<string, string> LoadCache()
     {
-        var json = File.ReadAllText(cacheFilePath);
-        return JsonConvert.DeserializeObject<Dictionary<string, string>>(json) ?? new Dictionary<string, string>();
+        var lines = File.ReadAllLines(cacheFilePath, Encoding.UTF8);
+        return ToDictionary(lines);
     }
 
     private static async ValueTask<Dictionary<string, string>> LoadCacheAsync(CancellationToken token)
     {
-        var json = await File.ReadAllTextAsync(cacheFilePath, token).ConfigureAwait(false);
-        return await Task.Run(() => JsonConvert.DeserializeObject<Dictionary<string, string>>(json) ?? new Dictionary<string, string>(), token).ConfigureAwait(false);
+        var lines = await File.ReadAllLinesAsync(cacheFilePath, Encoding.UTF8, token).ConfigureAwait(false);
+        return await Task.Run(() => ToDictionary(lines), token).ConfigureAwait(false);
     }
 
     private static void SaveCache(Dictionary<string, string> cacheData)
     {
-        var json = JsonConvert.SerializeObject(cacheData, Formatting.Indented);
-        File.WriteAllText(cacheFilePath, json);
+        var lines = cacheData.Select(kvp =>
+        {
+            return $"{kvp.Key}={kvp.Value}";
+        });
+        
+        File.WriteAllLines(cacheFilePath, lines, Encoding.UTF8);
     }
 
     private static async ValueTask SaveCacheAsync(Dictionary<string, string> cacheData, CancellationToken token)
     {
-        var json = await Task.Run(() => JsonConvert.SerializeObject(cacheData, Formatting.Indented), token).ConfigureAwait(false);
-        await File.WriteAllTextAsync(cacheFilePath, json, token).ConfigureAwait(false);
+        var lines = await Task.Run(() => cacheData.Select(kvp => $"{kvp.Key}={kvp.Value}"), token);
+        await File.WriteAllLinesAsync(cacheFilePath, lines, Encoding.UTF8, token).ConfigureAwait(false);
+    }
+
+    private static Dictionary<string, string> ToDictionary(string[] lines)
+    {
+        return new(lines.Select(l =>
+        {
+            var split = l.Split('=', 2, StringSplitOptions.None);
+            return new KeyValuePair<string, string>(split[0], split.Length > 1 ? split[1] : string.Empty);
+        }));
     }
 }
