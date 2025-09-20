43c456c084fcb0e2f85746790ccff10827270600
Christoph Hochstätter <Christoph@Hochstaetter.de>
2025-09-21 01:50:48 +0200

    Added WattPilotController

---
+++ HomeAutomationServer/Controllers/WattPilotController.cs
@@ -0,0 +1,25 @@
+﻿using De.Hochstaetter.Fronius.Models.Charging;
+using De.Hochstaetter.Fronius.Models.Gen24;
+using De.Hochstaetter.HomeAutomationServer.Models.Authorization;
+using Microsoft.AspNetCore.Http;
+
+namespace De.Hochstaetter.HomeAutomationServer.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+#pragma warning disable CS9107
+public class WattPilotController(IDataControlService controlService, ILogger<WattPilotController> logger) : DeviceControllerBase(controlService, logger)
+{
+    [HttpGet]
+    [BasicAuthorize(Roles = "User")]
+    [ProducesResponseType<IDictionary<string, Gen24System>>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status404NotFound)]
+    public IActionResult GetWattPilotDevices() => GetDevices<WattPilot>();
+
+    [HttpGet("{id}")]
+    [BasicAuthorize(Roles = "User")]
+    [ProducesResponseType<FritzBoxDevice>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status404NotFound)]
+    public IActionResult GetWattPilotDevice([FromRoute] string id) => GetDevice<WattPilot>(id);
+}
