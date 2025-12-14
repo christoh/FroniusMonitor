ff856b9d9e7e3101768236ba2786bc87f2692c62
Christoph Hochstätter <christoph@hochstaetter.de>
2025-12-14 17:06:09 +0100

    Some minor changes

--- Fronius/Services/BayernWerkImportService.cs
+++ Fronius/Services/BayernWerkImportService.cs
@@ -1,70 +1,69 @@
-﻿namespace De.Hochstaetter.Fronius.Services
+﻿namespace De.Hochstaetter.Fronius.Services;
+
+public class BayernWerkImportService(SettingsBase settings, IDataCollectionService dataCollectionService) : ISmartMeterImportService
 {
-    public class BayernWerkImportService(SettingsBase settings, IDataCollectionService dataCollectionService) : ISmartMeterImportService
+    public async Task ImportSmartMeterData(object parameters)
     {
-        public async Task ImportSmartMeterData(object parameters)
+        if (parameters is not string excelFileName)
         {
-            if (parameters is not string excelFileName)
-            {
-                throw new ArgumentException(string.Format(Resources.MustBeExcelFileName, nameof(parameters)), nameof(parameters));
-            }
-
-            IReadOnlyList<SmartMeterCalibrationHistoryItem> energyHistory;
-
-            _ = settings.EnergyHistoryFileName ?? throw new FileNotFoundException(Resources.NoEnergyHistoryFile);
-            _ = settings.DriftFileName ?? throw new FileNotFoundException(Resources.NoDriftFile);
-
-            await using (var fileStream = new FileStream(settings.EnergyHistoryFileName, FileMode.Open, FileAccess.Read, FileShare.Read))
-            {
-                var energyHistorySerializer = new XmlSerializer(typeof(SmartMeterCalibrationHistoryItem[]));
-
-                energyHistory = energyHistorySerializer.Deserialize(fileStream) as IReadOnlyList<SmartMeterCalibrationHistoryItem>
-                                ?? throw new InvalidDataException(string.Format(Resources.FileHasNoEnergyHistory, settings.EnergyHistoryFileName));
-            }
-
-            XLWorkbook workbook;
-
-            await using (var fileStream = new FileStream(excelFileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            {
-                workbook = new XLWorkbook(fileStream);
-            }
-
-            var sheet = workbook.Worksheet(1);
-            var obisCode = sheet.Cell("C4").Value.GetText();
-
-            var isProduction = obisCode switch
-            {
-                "1-1:1.29.0" => false,
-                "1-1:2.29.0" => true,
-                _ => throw new InvalidDataException("Incorrect OBIS code in Cell C4 of Excel file"),
-            };
-
-            var cell = sheet.Range("F:F").Cells()
-                           .Where(c => c.Value.IsText && c.GetValue<string>() == "VAL")
-                           .MaxBy(c => c.WorksheetRow().RowNumber())
-                       ?? throw new InvalidDataException(Resources.NoValidCells);
-
-            var row = cell.WorksheetRow();
-            var energy = row.Cell("G").GetValue<double>() * 1000;
-            var time = row.Cell("C").GetDateTime().ToUniversalTime();
-            var historyEntry = energyHistory.MinBy(i => Math.Abs(i.CalibrationDate.Ticks - time.Ticks));
-
-            if (historyEntry == null || Math.Abs((historyEntry.CalibrationDate - time).TotalMinutes) > 2)
-            {
-                throw new InvalidOperationException(Resources.NoEnergyHistoryMatch);
-            }
-
-            historyEntry.ProducedOffset = isProduction ? energy - historyEntry.EnergyRealProduced : double.NaN;
-            historyEntry.ConsumedOffset = isProduction ? double.NaN : energy - historyEntry.EnergyRealConsumed;
-
-            var lastEntry = dataCollectionService.SmartMeterHistory.LastOrDefault(i => double.IsFinite(isProduction ? i.ProducedOffset : i.ConsumedOffset));
-
-            if (lastEntry != null && lastEntry.CalibrationDate >= historyEntry.CalibrationDate)
-            {
-                throw new InvalidOperationException(Resources.ExcelFileAlreadyImported);
-            }
-
-            await dataCollectionService.AddCalibrationHistoryItem(historyEntry).ConfigureAwait(false);
+            throw new ArgumentException(string.Format(Resources.MustBeExcelFileName, nameof(parameters)), nameof(parameters));
         }
+
+        IReadOnlyList<SmartMeterCalibrationHistoryItem> energyHistory;
+
+        _ = settings.EnergyHistoryFileName ?? throw new FileNotFoundException(Resources.NoEnergyHistoryFile);
+        _ = settings.DriftFileName ?? throw new FileNotFoundException(Resources.NoDriftFile);
+
+        await using (var fileStream = new FileStream(settings.EnergyHistoryFileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+        {
+            var energyHistorySerializer = new XmlSerializer(typeof(SmartMeterCalibrationHistoryItem[]));
+
+            energyHistory = energyHistorySerializer.Deserialize(fileStream) as IReadOnlyList<SmartMeterCalibrationHistoryItem>
+                            ?? throw new InvalidDataException(string.Format(Resources.FileHasNoEnergyHistory, settings.EnergyHistoryFileName));
+        }
+
+        XLWorkbook workbook;
+
+        await using (var fileStream = new FileStream(excelFileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+        {
+            workbook = new XLWorkbook(fileStream);
+        }
+
+        var sheet = workbook.Worksheet(1);
+        var obisCode = sheet.Cell("C4").Value.GetText();
+
+        var isProduction = obisCode switch
+        {
+            "1-1:1.29.0" => false,
+            "1-1:2.29.0" => true,
+            _ => throw new InvalidDataException("Incorrect OBIS code in Cell C4 of Excel file"),
+        };
+
+        var cell = sheet.Range("F:F").Cells()
+                       .Where(c => c.Value.IsText && c.GetValue<string>() == "VAL")
+                       .MaxBy(c => c.WorksheetRow().RowNumber())
+                   ?? throw new InvalidDataException(Resources.NoValidCells);
+
+        var row = cell.WorksheetRow();
+        var energy = row.Cell("G").GetValue<double>() * 1000;
+        var time = row.Cell("C").GetDateTime().ToUniversalTime();
+        var historyEntry = energyHistory.MinBy(i => Math.Abs(i.CalibrationDate.Ticks - time.Ticks));
+
+        if (historyEntry == null || Math.Abs((historyEntry.CalibrationDate - time).TotalMinutes) > 2)
+        {
+            throw new InvalidOperationException(Resources.NoEnergyHistoryMatch);
+        }
+
+        historyEntry.ProducedOffset = isProduction ? energy - historyEntry.EnergyRealProduced : double.NaN;
+        historyEntry.ConsumedOffset = isProduction ? double.NaN : energy - historyEntry.EnergyRealConsumed;
+
+        var lastEntry = dataCollectionService.SmartMeterHistory.LastOrDefault(i => double.IsFinite(isProduction ? i.ProducedOffset : i.ConsumedOffset));
+
+        if (lastEntry != null && lastEntry.CalibrationDate >= historyEntry.CalibrationDate)
+        {
+            throw new InvalidOperationException(Resources.ExcelFileAlreadyImported);
+        }
+
+        await dataCollectionService.AddCalibrationHistoryItem(historyEntry).ConfigureAwait(false);
     }
 }
--- HomeAutomationClient/HomeAutomationClient/Controls/WifiControl.axaml.cs
+++ HomeAutomationClient/HomeAutomationClient/Controls/WifiControl.axaml.cs
@@ -1,58 +1,57 @@
-namespace De.Hochstaetter.HomeAutomationClient.Controls
+namespace De.Hochstaetter.HomeAutomationClient.Controls;
+
+public partial class WifiControl : Viewbox
 {
-    public partial class WifiControl : Viewbox
+    public static readonly StyledProperty<IBrush?> FillProperty = Shape.FillProperty.AddOwner<WifiControl>();
+
+    public IBrush? Fill
     {
-        public static readonly StyledProperty<IBrush?> FillProperty = Shape.FillProperty.AddOwner<WifiControl>();
+        get => GetValue(FillProperty);
+        set => SetValue(FillProperty, value);
+    }
 
-        public IBrush? Fill
+    public static readonly StyledProperty<double> SignalStrengthProperty = AvaloniaProperty.Register<WifiControl, double>(nameof(SignalStrength), -50d);
+    //new PropertyMetadata(-50d, (d, _) => ((WifiControl)d).OnSignalStrengthChanged())
+
+    public double SignalStrength
+    {
+        get => GetValue(SignalStrengthProperty);
+        set => SetValue(SignalStrengthProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> IsConnectedProperty = AvaloniaProperty.Register<WifiControl, bool>(nameof(IsConnected), true);
+
+    public bool IsConnected
+    {
+        get => GetValue(IsConnectedProperty);
+        set => SetValue(IsConnectedProperty, value);
+    }
+
+    public WifiControl()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        try
         {
-            get => GetValue(FillProperty);
-            set => SetValue(FillProperty, value);
-        }
-
-        public static readonly StyledProperty<double> SignalStrengthProperty = AvaloniaProperty.Register<WifiControl, double>(nameof(SignalStrength), -50d);
-        //new PropertyMetadata(-50d, (d, _) => ((WifiControl)d).OnSignalStrengthChanged())
-
-        public double SignalStrength
-        {
-            get => GetValue(SignalStrengthProperty);
-            set => SetValue(SignalStrengthProperty, value);
-        }
-
-        public static readonly StyledProperty<bool> IsConnectedProperty = AvaloniaProperty.Register<WifiControl, bool>(nameof(IsConnected), true);
-
-        public bool IsConnected
-        {
-            get => GetValue(IsConnectedProperty);
-            set => SetValue(IsConnectedProperty, value);
-        }
-
-        public WifiControl()
-        {
-            InitializeComponent();
-        }
-
-        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
-        {
-            try
+            switch (change.Property.Name)
             {
-                switch (change.Property.Name)
-                {
-                    case nameof(SignalStrength):
-                        High.Opacity = !IsConnected || SignalStrength < -52 ? 0.3 : 1;
-                        Medium.Opacity = !IsConnected || SignalStrength < -58 ? 0.3 : 1;
-                        Low.Opacity = !IsConnected || SignalStrength < -65 ? 0.3 : 1;
-                        break;
+                case nameof(SignalStrength):
+                    High.Opacity = !IsConnected || SignalStrength < -52 ? 0.3 : 1;
+                    Medium.Opacity = !IsConnected || SignalStrength < -58 ? 0.3 : 1;
+                    Low.Opacity = !IsConnected || SignalStrength < -65 ? 0.3 : 1;
+                    break;
 
-                    case nameof(IsConnected):
-                        Connection.Opacity = !IsConnected ? 0.3 : 1;
-                        break;
-                }
+                case nameof(IsConnected):
+                    Connection.Opacity = !IsConnected ? 0.3 : 1;
+                    break;
             }
-            finally
-            {
-                base.OnPropertyChanged(change);
-            }
+        }
+        finally
+        {
+            base.OnPropertyChanged(change);
         }
     }
 }
