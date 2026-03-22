0f1f20691bb23023d098de9d7468cb1312062e58
Christoph Hochstätter <christoph@hochstaetter.de>
2026-03-22 23:33:59 +0100

    Started mDNS tests

---
+++ Fronius/Services/MDnsService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace De.Hochstaetter.Fronius.Services
+{
+    internal class MDnsService
+    {
+    }
+}
---
+++ FroniusUnitTests/SystemTests/MDnsTests.cs
@@ -0,0 +1,60 @@
+﻿using Makaretu.Dns;
+using NUnit.Framework.Constraints;
+using System.Linq;
+using System.Net;
+
+namespace FroniusUnitTests.SystemTests;
+
+[TestFixture]
+public class MDnsTests
+{
+    [Test]
+    public async Task DiscoverWorks()
+    {
+        var sd = new ServiceDiscovery();
+        var mdns = new MulticastService();
+        mdns.AnswerReceived += OnAnswerReceived;
+
+        //sd.ServiceDiscovered += (s, serviceName) =>
+        //{
+        //    mdns.SendQuery(serviceName, type: DnsType.PTR);
+        //};
+
+        //sd.ServiceInstanceDiscovered += (s, e) =>
+        //{
+        //    mdns.SendQuery(e.ServiceInstanceName, type: DnsType.SRV);
+        //};
+
+        mdns.NetworkInterfaceDiscovered += OnInterfacesDiscovered;
+        mdns.Start();
+
+        await Task.Delay(TimeSpan.FromSeconds(10));
+
+        void OnInterfacesDiscovered(object? sender, NetworkInterfaceEventArgs e)
+        {
+            mdns.SendQuery(new DomainName("_fronius-se-inverter", "_tcp", "local"), type: DnsType.PTR);
+            mdns.SendQuery(new DomainName("_fronius-se-smartmeter", "_tcp", "local"), type: DnsType.PTR);
+            mdns.SendQuery(new DomainName("_fronius-se-wattpilot", "_tcp", "local"), type: DnsType.PTR);
+            mdns.SendQuery(new DomainName("_fronius-se-ohmpilot", "_tcp", "local"), type: DnsType.PTR);
+            mdns.SendQuery(new DomainName("_fbox", "_tcp", "local"), type: DnsType.PTR);
+            //sd.QueryAllServices();
+        }
+
+
+        void OnAnswerReceived(object? sender, MessageEventArgs e)
+        {
+            foreach (var record in e.Message.Answers.OfType<SRVRecord>())
+            {
+                TestContext.Out.WriteLine($"{record.Name} => {record.Target}");
+                TestContext.Out.Flush();
+                mdns.SendQuery(record.Target, type: DnsType.ANY);
+            }
+
+            foreach (var record in e.Message.Answers.OfType<AddressRecord>())
+            {
+                TestContext.Out.WriteLine($"{record.CanonicalName}=> {record.Address}");
+                TestContext.Out.Flush();
+            }
+        }
+    }
+}
