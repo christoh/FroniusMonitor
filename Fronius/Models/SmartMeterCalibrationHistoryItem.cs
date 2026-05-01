662ea8480a2eccc9b8f79c6006a626be690d3941
Christoph Hochstätter <christoph@hochstaetter.de>
2026-05-02 00:38:01 +0200

    Added default values (double.NaN) for Offsets in SmartMeterCalivrationHistoryItem

--- Fronius/Models/SmartMeterCalibrationHistoryItem.cs
+++ Fronius/Models/SmartMeterCalibrationHistoryItem.cs
@@ -15,14 +15,16 @@ public partial class SmartMeterCalibrationHistoryItem : BindableBase
     public partial DateTime CalibrationDate { get; set; }
 
     [ObservableProperty]
+    [DefaultValue(double.NaN)]
     [XmlAttribute("CrlOffset")]
-    public partial double ConsumedOffset { get; set; }
+    public partial double ConsumedOffset { get; set; } = double.NaN;
 
     [ObservableProperty]
+    [DefaultValue(double.NaN)]
     [XmlAttribute("PrlOffset")]
-    public partial double ProducedOffset { get; set; }
+    public partial double ProducedOffset { get; set; } = double.NaN;
 
-    #if DEBUG
-    public override string ToString()=>$"{CalibrationDate:g} P: {EnergyRealProduced} ({ProducedOffset}) / C: {EnergyRealConsumed} ({ConsumedOffset})";
-    #endif
+#if DEBUG
+    public override string ToString() => $"{CalibrationDate:g} P: {EnergyRealProduced} ({ProducedOffset}) / C: {EnergyRealConsumed} ({ConsumedOffset})";
+#endif
 }
