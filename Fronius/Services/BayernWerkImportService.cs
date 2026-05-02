3b2ac3f742eed8b949ba407fd1d95f48529df926
Christoph Hochstätter <christoph@hochstaetter.de>
2026-05-02 16:12:53 +0200

    Workaround for another bug in Bayernwerk Excel files

--- Fronius/Services/BayernWerkImportService.cs
+++ Fronius/Services/BayernWerkImportService.cs
@@ -41,7 +41,7 @@ public class BayernWerkImportService(SettingsBase settings, IDataCollectionServi
 
         var row = cell.WorksheetRow();
         var energy = row.Cell("G").GetValue<double>() * 1000;
-        var time = new DateTime(row.Cell("C").GetDateTime().Ticks, DateTimeKind.Utc);
+        var time = new DateTime(row.Cell("A").GetDateTime().Ticks, DateTimeKind.Utc);
         var historyEntry = energyHistory.MinBy(i => Math.Abs(i.CalibrationDate.Ticks - time.Ticks));
 
         if (historyEntry == null || Math.Abs((historyEntry.CalibrationDate - time).TotalMinutes) > 2)
